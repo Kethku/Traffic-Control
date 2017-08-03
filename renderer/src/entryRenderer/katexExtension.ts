8b378f172d1e7f1a9509a44d4b2e2e0be223c0a9
Keith Simmons <keith@the-simmons.net>
2017-08-03 10:10:57 -0700

    I dont remember

--- renderer/src/entryRenderer/katexExtension.ts
+++ renderer/src/entryRenderer/katexExtension.ts
@@ -3,33 +3,48 @@ import $ from "jquery";
 
 let katexCache: { [katex: string]: string } = {};
 
-export default {
-  type: 'output',
-  filter: function(html: string) {
-    if (!$('#katex-latex-styles').length) {
-      $('head').append(`
+function addLatexStyles() {
+      if (!$('#katex-latex-styles').length) {
+        $('head').append(`
 <style id="katex-latex-styles" type="text/css">
 .katex-latex {
-  font-size: 20px;
+font-size: 20px;
 }
 </style>`);
-    }
-
-    var $div = $('<div></div>').html(html);
-    var $latex = $('code.latex.language-latex', $div);
-
-    if ($latex.length) {
-      $latex.unwrap().each(function(i, e) {
-        var latexEl = $(e);
-        var htmlFromLatex = katex.renderToString(latexEl.text(), {
-          displayMode: true,
-          throwOnError: false,
-          errorColor: '#00c2c9'
-        });
-        latexEl.replaceWith(`<div class="katex-latex">${htmlFromLatex}</div>`);
-      });
-    }
-    console.log($div.html());
-    return $div.html();
-  }
+      }
 }
+
+export default [
+  {
+    type: 'lang',
+    regex: /\$([^]+?)\$/,
+    replace: function(s: any, match: any) {
+      addLatexStyles();
+      return `
+<span class="katex-latex">
+    ${katex.renderToString(match, {displayMode: true, throwOnError: false, errorColor: '#00c2c9'})}
+</span>`
+    }
+  }, {
+    type: 'output',
+    filter: function(html: string) {
+      addLatexStyles();
+      var $div = $('<div></div>').html(html);
+      var $latex = $('code.latex.language-latex', $div);
+
+      if ($latex.length) {
+        $latex.unwrap().each(function(i, e) {
+          var latexEl = $(e);
+          var htmlFromLatex = katex.renderToString(latexEl.text(), {
+            displayMode: true,
+            throwOnError: false,
+            errorColor: '#00c2c9'
+          });
+          latexEl.replaceWith(`<div class="katex-latex">${htmlFromLatex}</div>`);
+        });
+      }
+      console.log($div.html());
+      return $div.html();
+    }
+  }
+]
