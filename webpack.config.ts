9bfb513ab21a5fe855a252c8ab7e3ef0df70fd1a
Keith Simmons <t-kesimm@microsoft.com>
2017-09-12 11:58:54 -0700

    working on having file import dependent on the project

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,34 +1,43 @@
 import * as path from 'path';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as webpack from 'webpack';
+import { TsConfigPathsPlugin } from 'awesome-typescript-loader';
 
-const commonConfig: webpack.Configuration = {
-  context: path.resolve('.'),
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'babel-loader!awesome-typescript-loader',
-        exclude: path.resolve(__dirname, "node_modules")
+function generateConfig(options?: any) {
+  return {
+    context: path.resolve('.'),
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'babel-loader!awesome-typescript-loader',
+          exclude: path.resolve(__dirname, "node_modules"),
+          query: options
+        }, {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader',
+          exclude: path.resolve(__dirname, "node_modules")
+        }
+      ],
+      devServer: {
+        inline: true,
+        hot: true,
+        port: 8080
       },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader',
-        exclude: path.resolve(__dirname, "node_modules")
-      }
-    ]
-  },
-  devServer: {
-    inline: true,
-    hot: true,
-    port: 8080
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx', '.jsx']
-  },
-  node: {__dirname: false}
-};
+      devtool: "source-map",
+      resolve: {
+        extensions: ['.js', '.ts', '.tsx', '.jsx']
+      },
+      node: {__dirname: false}
+    }
+  }
+}
+
+function generateRendererConfig(options?: any) {
+  return Object.assign({
+    target: 'electron-renderer'
+  }, generateConfig(options));
+}
 
 const commonOutput: webpack.Output = {
   filename: '[name].js',
@@ -37,11 +46,7 @@ const commonOutput: webpack.Output = {
   }
 };
 
-const rendererConfig: webpack.Configuration = Object.assign({
-  target: 'electron-renderer'
-}, commonConfig);
-
-module.exports:  = [
+module.exports = [
   Object.assign({
     target: 'electron-main',
     entry: ['./main/main', 'babel-polyfill'],
@@ -49,23 +54,25 @@ module.exports:  = [
       path: path.resolve(__dirname, "build"),
       publicPath: '/'
     }, commonOutput)
-  }, commonConfig),
+  }, generateConfig({"include": [ "main\\**\\*" ]})),
   Object.assign({
     entry: ['./renderer/youtubePreload', 'babel-polyfill'],
     output: Object.assign({
       path: path.resolve(__dirname, "build/renderer"),
       publicPath: '/renderer/'
     }, commonOutput)
-  }, rendererConfig),
+  }, generateRendererConfig({"include": [ "renderer\\youtubePreload.ts" ]})),
   Object.assign({
     entry: ['./renderer/inputBox/inputBox', 'babel-polyfill'],
     output: Object.assign({
       path: path.resolve(__dirname, "build/renderer/inputBox"),
       publicPath: '/renderer/inputBox/'
     }, commonOutput),
-    plugins: [new HtmlWebpackPlugin({
-      title: 'Traffic Control Input',
-      filename: 'inputBox.html'
-    })]
-  }, rendererConfig)
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Traffic Control Input',
+        filename: 'inputBox.html'
+      })
+    ]
+  }, generateRendererConfig({"include": [ "renderer\\inputBox\\**\\*" ]}))
 ];
