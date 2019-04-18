dbf72112a5933a68b18debea98be8dc78c9b6d56
Keith Simmons <keith@the-simmons.net>
2019-04-18 13:59:20 -0700

    in progress porting to core

---
+++ TrafficControl/SniperManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TrafficControl {
+    public class SniperManager : IHandle<ProduceCompletionsEvent>, IHandle<InputEvent> {
+        public string SniperCommand = "snipe";
+
+        public SniperManager() 
+        {
+            Bootstrapper.EventAggregator.Subscribe(this);
+        }
+
+        public void Handle(ProduceCompletionsEvent message) {
+            if (CompletionUtils.Matches(message.Prefix, SniperCommand)) 
+            {
+                Bootstrapper.EventAggregator.PublishOnUIThread(new CompletionResultViewModel(SniperCommand, "snipe", "Arm sniper to punish deviant windows."));
+            }
+        }
+
+        public void Handle(InputEvent message) {
+            if (message.Input == SniperCommand)
+            {
+                Process.Start("window_sniper.exe");
+            }
+        }
+    }
+}
