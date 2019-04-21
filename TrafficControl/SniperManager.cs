59b6df2f3304789b8919baff5d2b55d0fd80785d
Keith Simmons <keith@the-simmons.net>
2019-04-20 21:08:08 -0700

    progress

--- TrafficControl/SniperManager.cs
+++ TrafficControl/SniperManager.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using Caliburn.Micro;
+using TrafficControl.ViewModels;
 
 namespace TrafficControl {
     public class SniperManager : IHandle<ProduceCompletionsEvent>, IHandle<InputEvent> {
